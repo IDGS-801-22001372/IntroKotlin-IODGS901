bf0efe0dae7c0960d15cf064b543ccbf63b6731f
IDGS-801-22001372 <81512@alumnos.utleon.edu.mx>
2025-05-12 21:05:00 -0600

    Trabajo en clase 12 de mayo

--- app/src/main/java/com/example/introkotlin_iodgs901/ejemplo2/SaludoActivity.kt
+++ app/src/main/java/com/example/introkotlin_iodgs901/ejemplo2/SaludoActivity.kt
@@ -1,8 +1,11 @@
 package com.example.introkotlin_iodgs901.ejemplo2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.introkotlin_iodgs901.R
@@ -12,10 +15,23 @@ class SaludoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_saludo)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val edtName = findViewById<AppCompatEditText>(R.id.edtName)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            val nombre = edtName.text.toString().trim()
+            if (nombre.isEmpty()) {
+                Toast.makeText(this, "Por favor, escribe tu nombre", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "¡Hola, $nombre!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
